096ca9b26da526910c0d6f06e57c5e3b2d76285e
Ítalo Alves <italooko@gmail.com>
2022-05-03 12:41:15 -0300

    Adiciona host do heroku

--- aplicacao/config/environments/development.rb
+++ aplicacao/config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Web host
+  config.hosts << "italo-alves-gces.herokuapp.com"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
